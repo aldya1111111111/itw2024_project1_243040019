4e156f18e10087f4c260550c95baf73e91ff0888
aldya11111111 <aldi.adriansyah2006@gmial.com>
2024-12-19 19:33:50 +0700

    portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".multiple-text", {
+    Strings: ["Unpas", "Faculty of Engineering","Informatics Engineering"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
+
+
+
